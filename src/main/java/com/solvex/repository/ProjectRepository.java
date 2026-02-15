2210f3344da33489490acd8f5212f202a499e95b
divin-d1 <rukundof993@gmail.com>
2026-02-15 12:00:00 +0000

    [Update]: Changes in project repository

--- src/main/java/com/solvex/repository/ProjectRepository.java
+++ src/main/java/com/solvex/repository/ProjectRepository.java
@@ -1,8 +1,6 @@
 package com.solvex.repository;
 import com.solvex.entity.Project;
 import com.solvex.entity.Project.Visibility;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import  org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 import java.util.Optional;
@@ -16,10 +14,12 @@ public interface ProjectRepository extends JpaRepository <Project,UUID>{
     Optional<Project> findById(UUID id);
 
     // find project by problem id
-    Page<Project> findByProblemId(UUID problemId, Pageable pageable);
+    List<Project> findByProblemIdAndVisibility(UUID problemId,Visibility visibility);
 
     // find project by owner id
-    Page<Project> findByOwnerId(UUID ownerId, Pageable pageable);
+    List<Project> findByOwnerId(UUID ownerId);
+
+    Optional<Project> findByIdAndOwnerId(UUID id,UUID ownerId);
 
     //find project by its visibility
     List <Project> findByVisibility(Visibility visibility);
