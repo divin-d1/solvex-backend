226da3d229317e7c6ecc2d2d934d3a9d11a7ffcb
divin-d1 <rukundof993@gmail.com>
2026-02-21 12:00:00 +0000

    [Update]: Handler problem controllers

--- src/main/java/com/solvex/controller/ProblemController.java
+++ src/main/java/com/solvex/controller/ProblemController.java
@@ -3,10 +3,12 @@ import com.solvex.dto.ProblemRequest;
 import com.solvex.entity.Problem;
 import com.solvex.service.ProblemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/problems")
@@ -14,17 +16,22 @@ import java.util.List;
 public class ProblemController {
     private final ProblemService problemService;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<?> createProblem(@RequestBody ProblemRequest request){
         Problem problem = problemService.createProblem(
                 request.getProblemTitle(),
                 request.getProblemDescription()
         );
 
-        return ResponseEntity.ok(problem);
+        return ResponseEntity
+                .status(HttpStatusCode.valueOf(201))
+                .body(Map.of(
+                        "success","Problem created",
+                        "problem", problem
+                ));
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public List<Problem> getProblems(){
         return problemService.getAllProblems();
     }
