8be42ddc19926983bf43a7116e3330748e0c637d
divin-d1 <rukundof993@gmail.com>
2026-02-23 12:00:00 +0000

    [Update]: Spring boot start class

--- src/main/java/com/solvex/SolveXApplication.java
+++ src/main/java/com/solvex/SolveXApplication.java
@@ -1,5 +1,6 @@
 package com.solvex;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -11,6 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SolveXApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().directory("E:/Big Projects/SolveX").load();
+        System.setProperty("spring.datasource.url",dotenv.get("DB_URL"));
+        System.setProperty("spring.datasource.username",dotenv.get("DB_USERNAME"));
+        System.setProperty("spring.datasource.password",dotenv.get("DB_PASSWORD"));
         SpringApplication.run(SolveXApplication.class, args);
     }
 }
